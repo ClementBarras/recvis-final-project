80b9afcb6d24a44a6aae27c37b53f5ee9c2fc8e3
Salma Tarmoun <salmatarmoun@gmail.com>
2019-01-02 16:03:40 +0000

    Added validation function

--- src/self_supervised_main.py
+++ src/self_supervised_main.py
@@ -6,11 +6,18 @@ import torch.optim as optim
 from dataset import ProxyTaskDataset
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
+import numpy as np
+from io import BytesIO
+import subprocess
+import sys
+import pandas as pd
 
 # Training settings
 parser = argparse.ArgumentParser(description='Self supervised learning script')
-parser.add_argument('--data', type=str, default='../datasets/UCF-101', metavar='D',
+parser.add_argument('--data', type=str, default='../datasets/UCF101_frames', metavar='D',
                     help="folder where data is located.")
+parser.add_argument('--video-list-directory', type=str, default='../datasets/ucfTrainTestlist', metavar='SD',
+                    help="directory where the video lists are stored")
 parser.add_argument('--sampling', type=str, default='random', metavar='SA,',
                     help="Sampling strategy (random, consecutive or constrained consecutive).")
 parser.add_argument('--n_questions', type=int, default=6, metavar='Q,',
@@ -43,8 +50,14 @@ if not os.path.isdir(args.experiment):
 # Data initialization and loading
 #from data import data_transforms
 
-train_set = ProxyTaskDataset(root=args.data, sampling=args.sampling, n_samples=args.n_samples, n_questions=args.n_questions)
+#train_split_paths = ['trainlist1.txt', 'trainlist2.txt', 'trainlist3.txt']
+#val_split_paths = ['vallist1.txt', 'vallist2.txt', 'vallist3.txt']
+
+train_set = ProxyTaskDataset(root=args.data, video_info_path=os.path.join(args.video_list_directory, 'completetrainlist.txt'),                                                sampling=args.sampling, n_samples=args.n_samples, n_questions=args.n_questions)
 train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
+
+validation_set = ProxyTaskDataset(root=args.data, video_info_path=os.path.join(args.video_list_directory, 'completetrainlist.txt'),                                                sampling=args.sampling, n_samples=args.n_samples, n_questions=args.n_questions)
+val_loader = DataLoader(validation_set, batch_size=args.batch_size, shuffle=True)
 
 #val_loader = torch.utils.data.DataLoader(
     #datasets.ImageFolder(args.data + '/val_images',
@@ -64,19 +77,33 @@ for layer in layers_to_freeze:
     for param in layer.parameters():
         param.requires_grad = False
 
+def get_free_gpu():
+    gpu_stats = subprocess.check_output(["nvidia-smi", "--format=csv", "--query-gpu=memory.used,memory.free"])
+    gpu_df = pd.read_csv(BytesIO(gpu_stats),
+                        names=['memory.used', 'memory.free'],
+                        skiprows=1)
+    print('GPU usage:\n{}'.format(gpu_df))
+    gpu_df['memory.free'] = gpu_df['memory.free'].map(lambda x: float(x.rstrip(' [MiB]')))
+    idx = gpu_df['memory.free'].idxmax()
+    print('Using GPU{} with {} free MiB'.format(idx, gpu_df.iloc[idx]['memory.free']))
+    return idx
+        
 if use_cuda:
     print('Using GPU')
-    model.cuda()
+    free_gpu_id = get_free_gpu()
+    device = "cuda:{}".format(free_gpu_id)
+    model.to(device)
 else:
     print('Using CPU')
 
-optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+#optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+optimizer = optim.Adam(model.parameters())
 
 def train(epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         if use_cuda:
-            data, target = data.float().cuda(), target.cuda()
+            data, target = data.float().to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
         criterion = torch.nn.CrossEntropyLoss(reduction='elementwise_mean')
@@ -87,11 +114,39 @@ def train(epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.data.item()))
+        
+            
+def validation():
+    model.eval()
+    validation_loss = 0
+    correct = 0
+    for data, target in val_loader:
+        if use_cuda:
+            data, target = data.to(device), target.to(device)
+        output = model(data)
+        # sum up batch loss
+        criterion = torch.nn.CrossEntropyLoss(reduction='elementwise_mean')
+        validation_loss += criterion(output, target).data.item()
+        # get the index of the max log-probability
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
+    validation_loss /= len(val_loader.dataset)
+    
+    print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        validation_loss, correct, len(val_loader.dataset),
+        100. * correct / len(val_loader.dataset)))
+    return validation_loss
 
+#sched = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor = np.sqrt(.1), patience = 2)
 for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    #validation()
+    training_loss = train(epoch)
+    #for param_group in optimizer.param_groups:
+       # print(param_group['lr'])
+    validation_loss =  validation()
+    #sched.step(validation_loss, epoch)
     model_file = args.experiment + '/model_' + str(epoch) + '.pth'
     torch.save(model.state_dict(), model_file)
     print('\nSaved model to ' + model_file + '.')
+            
+            
